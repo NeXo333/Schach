f4247b274fe8951d411f1896dd7e231eccc183da
NeXo333 <130662814+NeXo333@users.noreply.github.com>
2023-07-25 16:10:28 +0200

    Delete chess-game directory
    Neue projekt struktur. Hab das aktuelle einmal als backup gedownloaded. Gab keinen Weg das als ein Push zu machen. Hätte alles Schritt bei Schritt machen müssen. Ab jz dann nur noch einzelne files hochladen

--- chess-game/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.chess {
-    requires javafx.controls;
-    requires transitive javafx.graphics;
-
-    opens com.example.chess to javafx.fxml;
-
-    exports com.example.chess;
-}
