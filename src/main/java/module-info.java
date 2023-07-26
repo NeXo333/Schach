2b83f284dead683c2448e9b160b03989fef64772
NeXo333 <marlon.schrader30512464@gmail.com>
2023-07-26 10:37:46 +0200

    Neue Projekt Struktur: Neu: .gitignore, Primary/Secondary Controller, settings.json. Änderungen: Main Klasse wurde kurz gehalten, Auslagern der Methoden und main und start nur zum ausrufen von kurzen Befehlen um Klasse klein zu halten (App.java)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module thowl.model {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires transitive javafx.graphics;
+
+    opens thowl.model to javafx.fxml;
+    exports thowl.model;
+}
---
+++ src/main/java/thowl/model/Systeminfo.java
@@ -0,0 +1,12 @@
+package thowl.model;
+
+public class Systeminfo {
+    
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+}
