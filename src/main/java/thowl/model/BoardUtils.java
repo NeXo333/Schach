b9d1fac083f1f26663a71d6be9b32a008d319eca
NeXo333 <marlon.schrader30512464@gmail.com>
2023-08-09 10:52:11 +0200

    Board is start ready. All pieces now on the board. There is a movePiece method which can move pieces, currently only via Code. Next Steps are the implemetation of event handlers and then Game logic

--- src/main/java/thowl/model/BoardUtils.java
+++ src/main/java/thowl/model/BoardUtils.java
@@ -27,25 +27,42 @@ public class BoardUtils {
     // doesnt func
     chessboard.setPadding(new javafx.geometry.Insets(10));
 
-    for (int row = 0; row < 8; row++) {
+    // create white pieces on the top
+    createPiece(chessboard, 0, 0, Color.WHITE, "rook", "/images/whiteRook.png");
+    createPiece(chessboard, 0, 1, Color.WHITE, "knight", "/images/whiteKnight.png");
+    createPiece(chessboard, 0, 2, Color.WHITE, "bishop", "/images/whiteBishop.png");
+    createPiece(chessboard, 0, 3, Color.WHITE, "queen", "/images/whiteQueen.png");
+    createPiece(chessboard, 0, 4, Color.WHITE, "king", "/images/whiteKing.png");
+    createPiece(chessboard, 0, 5, Color.WHITE, "bishop", "/images/whiteBishop.png");
+    createPiece(chessboard, 0, 6, Color.WHITE, "knight", "/images/whiteKnight.png");
+    createPiece(chessboard, 0, 7, Color.WHITE, "rook", "/images/whiteRook.png");
+
+    // Place white pawns
+    for (int col = 0; col < 8; col++) {
+      createPiece(chessboard, 1, col, Color.WHITE, "pawn", "/images/whitePawn.png");
+    }
+
+    // Create black pieces on the buttom
+    createPiece(chessboard, 7, 0, Color.BLACK, "rook", "/images/blackRook.png");
+    createPiece(chessboard, 7, 1, Color.BLACK, "knight", "/images/blackKnight.png");
+    createPiece(chessboard, 7, 2, Color.BLACK, "bishop", "/images/blackBishop.png");
+    createPiece(chessboard, 7, 3, Color.BLACK, "queen", "/images/blackQueen.png");
+    createPiece(chessboard, 7, 4, Color.BLACK, "king", "/images/blackKing.png");
+    createPiece(chessboard, 7, 5, Color.BLACK, "bishop", "/images/blackBishop.png");
+    createPiece(chessboard, 7, 6, Color.BLACK, "knight", "/images/blackKnight.png");
+    createPiece(chessboard, 7, 7, Color.BLACK, "rook", "/images/blackRook.png");
+
+    // Place black pawns
+    for (int col = 0; col < 8; col++) {
+      createPiece(chessboard, 6, col, Color.BLACK, "pawn", "/images/blackPawn.png");
+    }
+
+    // Create empty cells for the rest of the chessboard
+    for (int row = 2; row <= 5; row++) {
       for (int col = 0; col < 8; col++) {
-        Color cellColor = (row + col) % 2 == 0 ? Color.WHITE : Color.LIGHTGRAY;
-
-        // Test for positioning of a pawn.
-        if (row == 1 || row == 6) {
-          String pieceName = "pawn";
-          String pieceImagePath = "/images/blackPawn.png"; // https://shorturl.at/cfnNV
-          Image pieceImage = new Image(getClass().getResourceAsStream(pieceImagePath));
-          cell[row][col] = new Cell(cellSize, cellColor, null, pieceName, pieceImage);
-
-          // Set the piece image for the cell
-          cell[row][col].setPieceImage(pieceImage);
-          chessboard.add(cell[row][col], col + 1, row + 1);
-
-        } else {
-          cell[row][col] = new Cell(cellSize, cellColor, null, null, null);
-          chessboard.add(cell[row][col], col + 1, row + 1);
-        }
+        Color cellColor = (row + col) % 2 == 0 ? Color.LIGHTGRAY : Color.WHITE;
+        cell[row][col] = new Cell(cellSize, cellColor, null, null, null);
+        chessboard.add(cell[row][col], col + 1, row + 1);
       }
     }
 
@@ -66,6 +83,49 @@ public class BoardUtils {
       chessboard.add(colIndex, col + 1, 0);
     }
 
+    // Test if you can move a piece
+    movePiece(cell, 1, 1, 3, 1);
+
     return chessboard; // Return the created chessboard GridPane
   }
+
+  private void createPiece(
+      GridPane chessboard,
+      int row,
+      int col,
+      Color pieceColor,
+      String pieceName,
+      String pieceImagePath) {
+    Color cellColor = (row + col) % 2 == 0 ? Color.LIGHTGRAY : Color.WHITE;
+    Image pieceImage = new Image(getClass().getResourceAsStream(pieceImagePath));
+
+    cell[row][col] = new Cell(cellSize, cellColor, pieceColor, pieceName, pieceImage);
+    cell[row][col].setPieceImage(pieceImage);
+
+    chessboard.add(cell[row][col], col + 1, row + 1);
+  }
+
+  // Testing with pieces moving:
+
+  public void movePiece(Cell[][] cellArray, int fromRow, int fromCol, int toRow, int toCol) {
+    Cell fromCell = cell[fromRow][fromCol];
+
+    // Get the piece values from the source cell
+    Color pieceColor = fromCell.getPieceColor();
+    String pieceName = fromCell.getPieceName();
+    Image pieceImage = fromCell.getPieceImage();
+
+    // Set the piece values in the destination cell
+    cellArray[toRow][toCol].setPieceValues(pieceColor, pieceName, pieceImage);
+    cell[toRow][toCol].setPieceImage(pieceImage);
+    // Clear the piece values in the source cell
+    cellArray[fromRow][fromCol].clearPiece();
+
+    // Update the appearance of the cells
+    cellArray[fromRow][toRow].updateCellAppearance();
+    cellArray[toRow][toCol].updateCellAppearance();
+  }
+
+  // ...
+
 }
--- src/main/java/thowl/model/Cell.java
+++ src/main/java/thowl/model/Cell.java
@@ -9,6 +9,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
 
 public class Cell extends StackPane {
+  public double cellSize = 70.0; // from BoardUtils. Wouldnt accept it direct from the other class
   private Color fieldColor;
   private Image pieceImage;
   private Color pieceColor;
@@ -29,10 +30,6 @@ public class Cell extends StackPane {
     background.setStrokeType(StrokeType.INSIDE); // Or StrokeType.OUTSIDE as needed
     background.setStrokeWidth(1.0);
 
-    // Set border properties
-    background.setStroke(Color.BLACK);
-    background.setStrokeWidth(1.0);
-
     // Create the ImageView for the piece image
     pieceImageView = new ImageView();
     pieceImageView.setFitWidth(size);
@@ -42,8 +39,64 @@ public class Cell extends StackPane {
     getChildren().addAll(background, pieceImageView);
   }
 
-  public void setPieceImage(Image image) {
-    pieceImageView.setImage(image);
+  // Add getter and setter methods for piece image
+  public Image getPieceImage() {
+    return pieceImageView.getImage();
+  }
+
+  public void setPieceImage(Image pieceImage) {
+    pieceImageView.setImage(pieceImage);
+  }
+
+  public Color getPieceColor() {
+    return pieceColor;
+  }
+
+  public void setPieceColor(Color pieceColor) {
+    this.pieceColor = pieceColor;
+  }
+
+  public String getPieceName() {
+    System.out.println(pieceName);
+    return pieceName;
+  }
+
+  public void setPieceName(String pieceName) {
+    this.pieceName = pieceName;
+  }
+
+  // big setter:
+  public void setPieceValues(Color pieceColor, String pieceName, Image pieceImage) {
+    this.pieceColor = pieceColor;
+    this.pieceName = pieceName;
+    this.pieceImage = pieceImage;
+  }
+
+  // clear cell
+  public void clearPiece() {
+    this.pieceColor = null;
+    this.pieceName = null;
+    this.pieceImage = null;
+    pieceImageView.setImage(null);
+  }
+
+  public void updateCellAppearance() {
+    Rectangle background = new Rectangle(cellSize, cellSize, fieldColor);
+    background.setStroke(Color.BLACK);
+    background.setStrokeType(StrokeType.INSIDE);
+    background.setStrokeWidth(1.0);
+
+    // Update the piece image if there is one
+    if (pieceImage != null) {
+      pieceImageView.setImage(pieceImage);
+    } else {
+      // If there is no piece image, clear the existing one
+      pieceImageView.setImage(null);
+    }
+
+    // Clear the children and add the updated background and piece image
+    getChildren().clear();
+    getChildren().addAll(background, pieceImageView);
   }
 
   // Could be used for eventhandler
